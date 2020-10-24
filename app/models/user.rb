656b294ea1369d47a3616c4cc2e3795ad7ea3760
JackQuinoa <jquinoa208@gmail.com>
2020-10-24 18:57:12 -0400

    users can now signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :anime, through: :reviews
+
+    validates :username, uniqueness: true, presence: true
+    
+    has_secure_password 
 end
