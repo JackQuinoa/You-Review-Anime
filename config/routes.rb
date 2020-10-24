da521e7183ca9a88e91c1b63a9781917f81097ad
JackQuinoa <jquinoa208@gmail.com>
2020-10-24 18:00:46 -0400

    added login/signup routes and updated welcome view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
   get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
   resources :reviews
   resources :animes
   resources :users
