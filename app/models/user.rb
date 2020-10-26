b18cff98b0b474da765b43a7f621cbb315005be4
JackQuinoa <jquinoa208@gmail.com>
2020-10-26 14:16:12 -0400

    fixed has_many relationship for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :anime, through: :reviews
+    has_many :animes, through: :reviews
 
     validates :username, uniqueness: true, presence: true
     
