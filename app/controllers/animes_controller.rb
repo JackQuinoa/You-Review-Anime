df18315016a29106f45648e429f01c8fee404b11
JackQuinoa <jquinoa208@gmail.com>
2020-10-26 22:59:14 -0400

    functional add an anime page

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,3 +1,23 @@
 class AnimesController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def new 
+        @anime = Anime.new
+    end
+
+    def create
+        @anime = Anime.new(anime_params)
+        if @anime.save
+            redirect_to animes_path
+        else
+            render :new 
+        end
+    end
+
+    private 
+
+    def anime_params
+        params.require(:anime).permit(:title)
+    end
 
 end
