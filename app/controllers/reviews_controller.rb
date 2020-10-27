849c97a92641317e35afcbac2137a8e2cfb4f2d7
JackQuinoa <jquinoa208@gmail.com>
2020-10-27 01:26:28 -0400

    functional nested anime's review page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
+    def index 
+        if params[:anime_id] && @anime = Anime.find_by_id(params[:anime_id])
+            @reviews = @anime.reviews 
+        else 
+            @reviews = Review.all 
+        end
+
+    end
+
 end
