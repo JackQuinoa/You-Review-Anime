bb83a05ca6714da166bca6613116e042bf7edd7d
JackQuinoa <jquinoa208@gmail.com>
2020-10-27 02:14:37 -0400

    working reviews index page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,4 +10,28 @@ class ReviewsController < ApplicationController
 
     end
 
+    def new
+        if params[:anime_id] && @anime = Anime.find_by_id(params[:anime_id])
+            @review = @anime.reviews.build
+        else
+            @review = Review.new 
+        end
+    end
+
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to reviews_path
+        else 
+            binding.pry
+            render :new 
+        end
+    end
+
+    private 
+
+    def review_params
+        params.require(:review).permit(:rating, :content, :anime_id)
+    end
+
 end
