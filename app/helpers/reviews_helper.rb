4d6dd68e825bcbb898fcb243e80286528739443c
JackQuinoa <jquinoa208@gmail.com>
2020-10-31 01:49:33 -0400

    used helper for new review view

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+    def new_display_header
+        if @review.anime
+            "Make a review for #{@review.anime.title}"
+        else
+            "New Review!"
+        end
+    end
+
+    
 end
